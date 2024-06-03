075ccf86e84d3e493dce36c2d31f52bfe9616a94
Jolo- <lockomoto@googlemail.com>
2024-06-03 21:02:57 +0100

    Firefox

--- src/service-worker.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse) => {
-    if (request.jwt) {
-        console.log('Token ::: ', request.jwt);
-        sendResponse({ success: true, message: 'Token has been received' });
-    }
-});
-
